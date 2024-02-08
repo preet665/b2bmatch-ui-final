daa7a207f27b48ac4fbbc3ee9ee2fde24f452177
Preet Thakkar <47426321+preet665@users.noreply.github.com>
2024-02-09 02:01:27 +0530

    Update promptGraph.tsx

--- components/promptGraph.tsx
+++ components/promptGraph.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-import Link from "next/link";
-import { Button } from "../components/ui/button";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const PromptGrph = () => {
+  const [htmlContent, setHtmlContent] = useState('');
+
+  useEffect(() => {
+    const fetchHtmlContent = async () => {
+      try {
+        const response = await axios.get('http://46.101.116.31:3000/get-treemap', {
+          responseType: 'text'
+        });
+        setHtmlContent(response.data);
+      } catch (error) {
+        console.error('Failed to fetch HTML content:', error);
+        // Handle error, e.g., set an error state, show a message, etc.
+      }
+    };
+
+    fetchHtmlContent();
+  }, []);
+
   return (
-    <>
-      <div className="w-fit rounded-lg  flex  text-lg mt-2 ">
-        <img src="/graph1.jpg" alt="" className="w-screen rounded-lg mr-3 " />
-      </div>
-    </>
+    <div className="w-fit rounded-lg flex text-lg mt-2">
+      <div
+        className="w-screen rounded-lg mr-3"
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
+      />
+    </div>
   );
 };
 
