61e051daa9a7fb2cf680e5ea5d9e06246cade880
raj <d22it187@charusat.edu.in>
2024-02-13 18:26:58 +0530

    pushed eye hide show change

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,14 +1,33 @@
 "use client";
 import { useState } from "react";
+import { Icon } from 'react-icons-kit';
+import { eyeOff } from 'react-icons-kit/feather/eyeOff';
+import { eye } from 'react-icons-kit/feather/eye';
+import Icon1 from './Icon';
+import CustomIcon from "./Icon";
+
 
 const LoginForm = ({ switchToSignupTab }) => {
+  const error = console.error;
+  console.error = (...args: any) => {
+    if (/defaultProps/.test(args[0])) return;
+    error(...args);
+  };
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const type = showPassword ? "text" : "password";
+  const icon = showPassword ? eye : eyeOff;
 
   const handleSignupClick = () => {
     switchToSignupTab();
     console.log("signup clicked");
   };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <section className="shadow-none border-none outline-none">
       <div className="flex flex-col items-center justify-center h-full ">
@@ -35,19 +54,47 @@ const LoginForm = ({ switchToSignupTab }) => {
               </div>
               <div>
                 <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Nutzername
+                </label>
+                <input
+                  type="username"
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Nike"
+                />
+              </div>
+              <div>
+                <label
                   htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Passwort
                 </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                />
+                <div className="flex bg-white rounded-lg border-2 border-gray-300">
+                  <input
+                    type={type}
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 border-none text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="relative transform -translate-y-1/2 text-gray-400 focus:outline-none m-1"
+                  >
+                    <CustomIcon icon={icon} size={20} />
+                  </button>
+                </div>
+
+
+
               </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
--- components/promptGraph.tsx
+++ components/promptGraph.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { Button } from "../components/ui/button";
+import axios from "axios";
 
 const PromptGrph = () => {
+  const [htmlContent, setHtmlContent] = useState('');
+
+  useEffect(() => {
+    const fetchHtmlContent = async () => {
+      try {
+        const response = await axios.get('http://46.101.116.31:3000/get-treemap', {
+          responseType: 'text'
+        });
+        setHtmlContent(response.data);
+      } catch (error) {
+        console.error('Failed to fetch HTML content:', error);
+        // Handle error, e.g., set an error state, show a message, etc.
+      }
+    };
+
+    fetchHtmlContent();
+  }, []);
   return (
     <>
-      <div className="w-fit rounded-lg  flex  text-lg mt-2 ">
+      <div className="w-[100%] rounded-lg  flex  text-lg mt-2 overflow-scroll ">
+        <div
+          dangerouslySetInnerHTML={{ __html: htmlContent }}
+        />
         <img src="/graph1.jpg" alt="" className="w-screen rounded-lg mr-3 " />
       </div>
     </>
